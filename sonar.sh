b9ef8fcdb1f1e219d1d1c2e7705d08d3a02855c4
VINAYSV3102 <114980298+VINAYSV3102@users.noreply.github.com>
2024-06-20 23:50:15 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 #Launch an instance with 9000 and t2.medium
 cd /opt/
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.5.1 .zip
-unzip sonarqube-10.5.1 .zip
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.5.50698.zip .zip
+unzip sonarqube-8.9.5.50698.zip .zip
 amazon-linux-extras install java-openjdk11 -y
 useradd sonar
-chown sonar:sonar sonarqube-10.5.1  -R
-chmod 777 sonarqube-10.5.1  -R
+chown sonar:sonar sonarqube-8.9.5.50698.zip  -R
+chmod 777 sonarqube-8.9.5.50698.zip  -R
 su - sonar
 
 #run this on server manually
-#sh /opt/sonarqube-10.5.1 /bin/linux/sonar.sh start
+#sh /opt/sonarqube-8.9.5.50698.zip /bin/linux/sonar.sh start
 #echo "user=admin & password=admin"
